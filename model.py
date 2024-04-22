ea3aaa0f4a703db4b0313d793e7c97f7ca934215
Merlin Lüdicke <merlin.luedicke@gmail.com>
2024-04-22 20:06:39 +0200

    feat: remove defaults from GPTconfig

--- model.py
+++ model.py
@@ -117,15 +117,13 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 1024
-    # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    vocab_size: int = 50304
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-    dropout: float = 0.0
-    # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-    bias: bool = True
+    block_size: int
+    vocab_size: int  # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int
+    n_head: int
+    n_embd: int
+    dropout: float
+    bias: bool  # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 
 class GPT(nn.Module):
