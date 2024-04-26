73ecd2b6982b70ee5fedde41d30b71ff0a8a1d2b
Merlin Lüdicke <merlin.luedicke@gmail.com>
2024-04-26 23:36:09 +0200

    chore: cleanup stuff

--- tests/test_train.py
+++ tests/test_train.py
@@ -64,7 +64,6 @@ def _log() -> None:
         f.write("\n")
 
 
-# @pytest.mark.flaky()
 @pytest.mark.usefixtures("_log")
 @pytest.mark.parametrize(
     ("results", "expected"),
@@ -100,55 +99,6 @@ def _log() -> None:
     ],
     indirect=["results"],
 )
-# WTF does it fail:
-# 2024-04-26 22:35:55.254473 [Result(iter=0, loss=4.236855, time=290.48), Result(iter=80, loss=2.549376, time=8.55)]
-# 2024-04-26 22:35:57.801171 [Result(iter=0, loss=4.210068, time=291.75), Result(iter=80, loss=2.570952, time=8.9)]
-# 2024-04-26 22:36:00.411026 [Result(iter=0, loss=4.176258, time=297.74), Result(iter=80, loss=2.575707, time=9.62)]
-# 2024-04-26 22:36:08.269912 [Result(iter=0, loss=4.214901, time=5696.1), Result(iter=80, loss=2.526675, time=4.55)]
-
-
-# post optimization, pre statistic filtering:
-# 2024-04-26 20:48:41.773429 [Result(iter=0, loss=4.236855, time=290.32), Result(iter=80, loss=2.549376, time=8.61)]
-# 2024-04-26 20:48:44.344302 [Result(iter=0, loss=4.210068, time=294.82), Result(iter=80, loss=2.570952, time=8.96)]
-# 2024-04-26 20:48:46.960198 [Result(iter=0, loss=4.176258, time=299.05), Result(iter=80, loss=2.575707, time=9.64)]
-# 2024-04-26 20:48:54.876659 [Result(iter=0, loss=4.2149 , time=5739.05), Result(iter=80, loss=2.526495, time=4.52)]
-
-# @pytest.mark.usefixtures("_log")
-# @pytest.mark.parametrize(
-#     ("results", "expected"),
-#     [
-#         (
-#             "tests/config/config_1.py",
-#             [
-#                 Result(iter=0, loss=4.236855, time=265),
-#                 Result(iter=80, loss=3.037239, time=22),
-#             ],
-#         ),
-#         (
-#             "tests/config/config_2.py",
-#             [
-#                 Result(iter=0, loss=4.210068, time=265),
-#                 Result(iter=80, loss=3.100468, time=22),
-#             ],
-#         ),
-#         (
-#             "tests/config/config_3.py",
-#             [
-#                 Result(iter=0, loss=4.176258, time=265),
-#                 Result(iter=80, loss=3.006874, time=22),
-#             ],
-#         ),
-#         (
-#             "tests/config/config_4.py",
-#             [
-#                 Result(iter=0, loss=4.214890, time=5200),
-#                 Result(iter=80, loss=2.93995, time=11),
-#             ],
-#         ),
-#     ],
-#     indirect=["results"],
-# )
-
 def test_train(results: list[Result], expected: list[Result]) -> None:
     assert len(results) == len(expected)
 
